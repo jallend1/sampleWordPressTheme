068a502719ac6721cb6d7f4aa8a23a7f7eedf731
jallend1 <jallend1@gmail.com>
2021-10-26 15:05:43 -0700

    Creates basic page for single posts

---
+++ single.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<?php while(have_posts()){ ?>
+    <?php the_post(); ?>
+    <h2><?php the_title() ?></h2>
+    <p><?php the_content() ?></p>
+<?php } ?>    
+<?php get_footer(); ?>
