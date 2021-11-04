677cad0adc27575fea9fba9d27c2814d570f2c62
jallend1 <jallend1@gmail.com>
2021-11-03 17:47:19 -0700

    Adds dummy events and blog content to index page

--- index.php
+++ index.php
@@ -10,5 +10,33 @@
         <button>Join</button>
     </div>
 </div>
+<section class="sections">
+    <div class="events">
+        <h2>Event Calendar</h2>
+        <div>
+            <div>
+                <h3>A fake event</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti asperiores corporis, culpa aspernatur cupiditate neque iste dolorem rem blanditiis necessitatibus!</p>
+            </div>
+            <div>
+                <h3>Another fake event</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti asperiores corporis, culpa aspernatur cupiditate neque iste dolorem rem blanditiis necessitatibus!</p>
+            </div>
+        </div>
+    </div>
+    <div class="blogs">
+        <h2>Our Latest News</h2>
+        <div>
+            <div>
+                <h3>A fake blog</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti asperiores corporis, culpa aspernatur cupiditate neque iste dolorem rem blanditiis necessitatibus!</p>
+            </div>
+            <div>
+                <h3>Another fake blog</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti asperiores corporis, culpa aspernatur cupiditate neque iste dolorem rem blanditiis necessitatibus!</p>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
