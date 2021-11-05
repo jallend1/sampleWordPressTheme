e7b99d4d7a56cef2926fa7d6f34fe7f80d2577f2
jallend1 <jallend1@gmail.com>
2021-11-04 18:28:54 -0700

    Adds title tag support to WordPress theme

--- functions.php
+++ functions.php
@@ -1,11 +1,16 @@
 <?php 
 
-function customStyles() {
+function libraryUnionFeatures(){
+    add_theme_support('title-tag');
+}
+
+function libraryUnionStyles() {
     wp_enqueue_style('fontAwesome', "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
     wp_enqueue_style('googleFonts', "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
     wp_enqueue_style('localStyles', get_stylesheet_uri());
 }
 
-add_action('wp_enqueue_scripts', 'customStyles');
+add_action('wp_enqueue_scripts', 'libraryUnionStyles');
+add_action('init', 'libraryUnionFeatures');
 
 ?>
