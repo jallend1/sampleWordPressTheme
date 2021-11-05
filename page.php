c7557f67f60c6d1d676d511d642143939617b65a
jallend1 <jallend1@gmail.com>
2021-11-04 18:21:48 -0700

    Adds simple little navbar menu to page template

--- page.php
+++ page.php
@@ -3,7 +3,15 @@
     <h1 class="heading"><?php the_title(); ?></h1>
     <h2 class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime iure deleniti veritatis molestiae repudiandae earum repellat?</h2>
 </div>
-<div>
+<div class="page-content">
+<nav class="page-nav">
+    <div>
+        <p>Home</p>
+    </div>
+    <div>
+        <p><?php the_title(); ?></p>
+    </div>
+</nav>
     <?php the_content(); ?>
 </div>
     
