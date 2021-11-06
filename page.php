e212c17d4d4a423227a9b3f67c7eb1666e3a1857
jallend1 <jallend1@gmail.com>
2021-11-06 15:30:11 -0700

    Displays breadcrumb menu on pages with parents

--- page.php
+++ page.php
@@ -4,16 +4,21 @@
     <h2 class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime iure deleniti veritatis molestiae repudiandae earum repellat?</h2>
 </div>
 <div class="page-content">
-<nav class="page-nav">
-    <div>
-        <a href="<?php site_url(); ?>">    
-            <p>Home</p>
-        </a>
-        </div>
-    <div>
-        <p><?php the_title(); ?></p>
-    </div>
-</nav>
+<!-- If child has a parent page, display breadcrumbs -->
+<?php
+    $parentPost = wp_get_post_parent_id(get_the_ID());
+    if($parentPost){ ?>
+            <nav class="page-nav">
+            <div>
+                <a href="<?php echo get_permalink($parentPost); ?>">    
+                    <p><?php echo get_the_title($parentPost); ?></p>
+                </a>
+                </div>
+            <div>
+                <p><?php the_title(); ?></p>
+            </div>
+        </nav>
+    <?php } ?>
     <?php the_content(); ?>
 </div>
     
