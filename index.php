f1bbe1b656c2644e0e662f390839e3e132bab12a
jallend1 <jallend1@gmail.com>
2021-11-07 12:12:16 -0800

    Adds header and post details to individual blog page

--- index.php
+++ index.php
@@ -7,22 +7,24 @@
 <div class="page-content">
     <?php while(have_posts()){
         the_post(); ?>
-    <div class="blog-post">
-        <h3 class="blog-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-        <div class="blog-details">
-            <p>Posted by 
-                <?php the_author_posts_link(); ?> 
-                on <?php the_time('F j, Y'); ?> 
-                in <?php echo get_the_category_list(', '); ?>.
-            </p>
+        <div class="blog-post">
+            <h3 class="blog-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <div class="blog-details">
+                <p>Posted by 
+                    <?php the_author_posts_link(); ?> 
+                    on <?php the_time('F j, Y'); ?> 
+                    in <?php echo get_the_category_list(', '); ?>.
+                </p>
+            </div>
+            <div class="blog-content">
+                <?php the_excerpt(); ?>
+                <p><a href="<?php echo the_permalink(); ?>">Continue reading &raquo; </a></p>
+            </div>
         </div>
-        <div class="blog-content">
-        <?php the_excerpt(); ?>
-        <p><a href="<?php echo the_permalink(); ?>">Continue reading &raquo; </a></p>
-        </div>
-</div>
-
-    <?php } ?>
+    <?php } 
+        echo paginate_links();
+    ?>
+    
 </div>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,10 +1,30 @@
-<?php 
-    get_header(); 
-    
-    while(have_posts()){ 
-        the_post(); ?>
-        <h2><?php the_title() ?></h2>
-        <p><?php the_content() ?></p>
+<?php get_header();  ?>
+
+<?php while(have_posts()){ 
+    the_post(); ?>
+    <div class="page-banner">
+        <h1 class="heading"><?php the_title(); ?></h1>
+        <h2 class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime iure deleniti veritatis molestiae repudiandae earum repellat?</h2>
+    </div>
+    <div class="page-content">
+        <nav class="page-nav">
+            <div>
+                <a href="<?php echo site_url('/blog'); ?>">    
+                    <p>Blog Home</p>
+                </a>
+            </div>
+            <div class="blog-details">
+                <p>Posted by 
+                    <?php the_author_posts_link(); ?> 
+                    on <?php the_time('F j, Y'); ?> 
+                    in <?php echo get_the_category_list(', '); ?>.
+                </p>
+            </div>
+        </nav>
+        <div class="blog-content">    
+            <?php the_content(); ?>    
+        </div>
+    </div>
     <?php } 
 
     get_footer(); 
