db20bdd64b9ac12f9e7f275096e52138e0d5fc01
jallend1 <jallend1@gmail.com>
2021-11-09 17:24:29 -0800

    Adds event custom post type

--- functions.php
+++ functions.php
@@ -1,20 +1,31 @@
 <?php 
 
-function libraryUnionFeatures(){
+function library_union_features(){
     add_theme_support('title-tag');
     register_nav_menu('headerMenu', 'Header Menu');
     register_nav_menu('footerOne', 'Footer Location 1');
     register_nav_menu('footerTwo', 'Footer Location 2');
 }
 
-function libraryUnionStyles() {
+function library_union_post_types(){
+    register_post_type('event', array(
+        'public' => true,
+        'menu_icon' => 'dashicons-calendar',
+        'labels' => array(
+            'name' => 'Events'
+        )
+    ));
+}
+
+function library_union_styles() {
     wp_enqueue_style('fontAwesome', "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
     wp_enqueue_style('googleFonts', "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
     wp_enqueue_style('localStyles', get_stylesheet_uri());
 }
 
-add_action('wp_enqueue_scripts', 'libraryUnionStyles');
-add_action('after_setup_theme', 'libraryUnionFeatures');
+add_action('wp_enqueue_scripts', 'library_union_styles');
+add_action('after_setup_theme', 'library_union_features');
+add_action('init', 'library_union_post_types');
 
 ?>
 
